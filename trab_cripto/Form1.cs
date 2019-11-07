8a2b72becb40a32591d636eff03bfbf6c050aaee
ghgomes41 <ghgomes279@gmail.com>
2019-11-07 15:11:37 -0300

    Fix the character table

--- trab_cripto/Form1.cs
+++ trab_cripto/Form1.cs
@@ -41,9 +41,6 @@ namespace trab_cripto
         private void Exec_btn1_Click(object sender, EventArgs e)
         {
             String crypt_src = txt_src1.Text.ToUpper();
-            // String crypt_target = crypt_src;
-            //.Replace('0','o').Replace('1','p').Replace('2','q').Replace('3','r')
-            //.Replace('4', 'ó').Replace('5', 'ú').Replace('6', 'à').Replace('7', 'ô').Replace('8', 'ã').Replace('9', 'õ')
             String crypt_target = crypt_src.Replace('0', 'o').Replace('1', 'p').Replace('2', 'q').Replace('3', 'r')
                                            .Replace('4', 'ó').Replace('5', 'ú').Replace('6', 'à').Replace('7', 'ô').Replace('8', 'ã').Replace('9', 'õ')
                                            .Replace('A', '4').Replace('B', '5').Replace('C', '6').Replace('D', '7').Replace('E', '8').Replace('F', '9')
@@ -55,8 +52,8 @@ namespace trab_cripto
                                            .Replace('!', '%').Replace('@', '&').Replace('#', '*').Replace('$', '(').Replace('_', ')').Replace('+', '-')
                                            .Replace('|', '!').Replace('?', '@').Replace('=', '#').Replace('´', '$').Replace('`', '_').Replace(':', '+')
                                            .Replace('.', 'y').Replace(',', 'z').Replace(';', '0').Replace('Á', '1').Replace('É', '2').Replace('Í', '3')
-                                           .Replace('Ê', '.').Replace('Â', ',').Replace('Ô', ';').Replace('Ç', 'á').Replace('Û', 'é').Replace('Ü', 'í')
-                                           .Replace('Ñ', 'ê').Replace('Ö', 'â').Replace('Ä', 'ô').Replace('£', 'ç').Replace('º', 'û').Replace('ª', 'ü')
+                                           .Replace('Ê', '.').Replace('Â', ',').Replace('¢', ';').Replace('Ç', 'á').Replace('Û', 'é').Replace('Ü', 'í')
+                                           .Replace('Ñ', 'ê').Replace('Ö', 'â').Replace('Ä', '¢').Replace('£', 'ç').Replace('º', 'û').Replace('ª', 'ü')
                                            .Replace('[', 'ñ').Replace(']', 'ö').Replace('{', 'ä').Replace('}', '£').Replace('~', 'º').Replace('^', 'ª')
                                            .Replace('Ó', '[').Replace('Ú', ']').Replace('À', '{').Replace('Ô', '}').Replace('Ã', '~').Replace('Õ', '^');
                                            
@@ -75,10 +72,10 @@ namespace trab_cripto
                                                .Replace('!', '|').Replace('@', '?').Replace('#', '=').Replace('$', '´').Replace('_', '`').Replace('+', ':')
                                                .Replace('%', '!').Replace('&', '@').Replace('*', '#').Replace('(', '$').Replace(')', '_').Replace('-', '+')
                                                .Replace('[', 'Ó').Replace(']', 'Ú').Replace('{', 'À').Replace('}', 'Ô').Replace('~', 'Ã').Replace('^', 'Õ')
-                                               .Replace('.', 'Ê').Replace(',', 'Â').Replace(';', 'Ô').Replace('á', 'Ç').Replace('é', 'Û').Replace('í', 'Ü')
+                                               .Replace('.', 'Ê').Replace(',', 'Â').Replace(';', '¢').Replace('á', 'Ç').Replace('é', 'Û').Replace('í', 'Ü')
                                                .Replace('y', '.').Replace('z', ',').Replace('0', ';').Replace('1', 'Á').Replace('2', 'É').Replace('3', 'Í')
                                                .Replace('ñ', '[').Replace('ö', ']').Replace('ä', '{').Replace('£', '}').Replace('º', '~').Replace('ª', '^')
-                                               .Replace('ê', 'Ñ').Replace('â', 'Ö').Replace('ô', 'Ä').Replace('ç', '£').Replace('û', 'º').Replace('ü', 'ª')
+                                               .Replace('ê', 'Ñ').Replace('â', 'Ö').Replace('¢', 'Ä').Replace('ç', '£').Replace('û', 'º').Replace('ü', 'ª')
                                                .Replace('o', '0').Replace('p', '1').Replace('q', '2').Replace('r', '3')
                                                .Replace('ó', '4').Replace('ú', '5').Replace('à', '6').Replace('ô', '7').Replace('ã', '8').Replace('õ', '9');
                                                
